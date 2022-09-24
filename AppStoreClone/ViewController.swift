6a00e0b8c43f3e1f31e9dc34d8a0eff03a4514c1
Ensar Ozturk <ozturkkensar@gmail.com>
2022-09-24 13:18:07 +0300

    Initial Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Ensar Ozturk on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
