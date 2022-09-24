9fc21ee4b94e5a6e06825fea8fc1d2060709e171
Ensar Ozturk <ozturkkensar@gmail.com>
2022-09-24 16:51:29 +0300

    add collectionView register AppListCell, show sample data on collecitonView

--- AppStoreClone/Modules/AppList/AppListViewController.swift
+++ AppStoreClone/Modules/AppList/AppListViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class AppListViewController: UIViewController {
-       
+class AppListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
     @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: "AppListCell", bundle: nil), forCellWithReuseIdentifier: "AppListCell")
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AppListCell", for: indexPath) as! AppListCell
+        
+        cell.nameLabel.text = "Ensar"
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGSize(width: collectionView.frame.width, height: 96)
+        
+        return size
     }
 }
+
